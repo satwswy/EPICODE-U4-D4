d9f6ebe3193ee72fe2b6c169487a978fb46367b8
marios <mgeorgak859@gmail.com>
2022-07-21 17:54:55 +0300

    added files folder

--- src/server.js
+++ src/server.js
@@ -2,14 +2,20 @@ import express from "express"
 import blogPostsRouter from "./apis/blogs/index.js"
 import listEndpoints from "express-list-endpoints"
 import { badRequestHandler, genericServerErrorHandler, notFoundHandler, unauthorizedHandler } from "./errorHandlers.js"
+import filesRouter from "./apis/files/index.js"
+import {join} from "path"
+import cors from "cors"
 
 const server = express()
 
 const port = 3002
+const publicFolderPath = join(process.cwd(), "./public")
+server.use(express.static(publicFolderPath))
 server.use(cors())
 server.use(express.json())
 
 server.use("/blogPosts",blogPostsRouter )
+server.use("/files", filesRouter)
 
 server.use(badRequestHandler)
 server.use(unauthorizedHandler)
