ce85f6300a6655014f700c81fbf9108b172a7984
marios <mgeorgak859@gmail.com>
2022-07-21 17:38:54 +0300

    added blogPosts pfp

--- src/lib/fs-tools.js
+++ src/lib/fs-tools.js
@@ -6,6 +6,9 @@ const {readJSON, writeJSON, writeFile} = fs
 
 const dataFolderPath = join(dirname(fileURLToPath(import.meta.url)),"../data")
 const blogPostsJSONPath = join(dataFolderPath, "blogPosts.json")
+const blogPostsPublicFolderPath = join(process.cwd(), "./public/img/blogPosts")
 
 export const getBlogPosts = () => readJSON(blogPostsJSONPath)
 export const writeBlogPosts = blogPostsArray => writeJSON(blogPostsJSONPath, blogPostsArray) 
+
+export const saveBlogPostsCovers = (fileName, contentAsABuffer) => writeFile(join(blogPostsPublicFolderPath, fileName), contentAsABuffer)
