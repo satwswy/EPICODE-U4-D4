efc452572135e1d22751337c645f06c4a9f3fc04
marios <mgeorgak859@gmail.com>
2022-07-21 17:50:10 +0300

    added files folder

---
+++ src/apis/files/index.js
@@ -0,0 +1,37 @@
+import express from "express";
+import multer from "multer";
+import { extname } from "path";
+import { saveBlogPostsCovers } from "../../lib/fs-tools";
+
+const filesRouter = express.Router();
+
+filesRouter.post(
+  "/single",
+  multer({}).single("cover"),
+  async (req, res, next) => {
+    try {
+      console.log("File: ", req.file);
+      const fileName = extname(req.file.buffer);
+      res.send("uploaded");
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+filesRouter.post(
+  "/multiple",
+  multer().array("covers"),
+  async (req, res, next) => {
+    try {
+      console.log("Files: ", req.files);
+      const arrayOfPromises = req.files.map((file) =>
+        saveUsersAvatars(file.originalname, file.buffer)
+      );
+      await Promise.all(arrayOfPromises);
+      res.send("UPLOADED");
+    } catch (error) {
+      next(error);
+    }
+  }
+);
