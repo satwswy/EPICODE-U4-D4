3cba74f19f40e345ecd55b29722685bf6d66d954
marios <mgeorgak859@gmail.com>
2022-07-26 22:27:23 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -51,7 +51,7 @@ server.use(catchAllErrorHandler);
 
 console.log(listEndpoints(server));
 
-server.listen(PORT, () => console.log("✅ Server is running on port : ", PORT));
+server.listen(port, () => console.log("✅ Server is running on port : ", port));
 
 server.on("error", (error) =>
   console.log(`❌ Server is not running due to : ${error}`)
